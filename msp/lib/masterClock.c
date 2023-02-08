6db88260453e997b57368e921e821b25a9596304
Ast3roidPotato <consaljj@rose-hulman.edu>
2023-02-08 14:37:09 -0500

    edited master clock

--- msp/lib/masterClock.c
+++ msp/lib/masterClock.c
@@ -1,9 +1,16 @@
 #include "masterClock.h"
+#include "../csHFXT.h"
 
 volatile uint64_t clockTick = 0;
 
 void init() {
     // do init things
+    configHFXT();
+    //Timer_A0 in continuous mode at 1mhz
+    TIMER_A0->CTL = TIMER_A_CTL_SSEL__SMCLK | TIMER_A_CTL_MC__CONTINUOUS;
+    TIMER_A0->CCTL[0] = TIMER_A_CCTLN_CCIE;
+    
+
 }
 
 static uint64_t now() { return clockTick; }
