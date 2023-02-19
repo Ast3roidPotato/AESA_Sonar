841bd4cafc7527d5d951101c922199dfebfdfb0a
Evelyn Elmer <PeaceLoveScience2@users.noreply.github.com>
2023-02-19 14:18:28 -0500

    changed so counts length of pos width of one pin

--- msp/main.c
+++ msp/main.c
@@ -23,7 +23,7 @@ void main(void) {
  while(true) {
     if (((LISTEN_PIN_PORT -> IN) & LISTEN_PIN) == LISTEN_PIN) {
         startTimer();
-        while (((CAPTURE_PIN_PORT -> IN) & CAPTURE_PIN) != CAPTURE_PIN) {
+        while (((LISTEN_PIN_PORT -> IN) & LISTEN_PIN) == LISTEN_PIN) {
 
         }
         stopTimer();
